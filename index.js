9f8adf04b4a9301109c1bc6baf82d4baa1c54092
Pankaj Singh Parihar <pankajsinghparihar211@gmail.com>
2023-09-23 17:33:45 +0530

    setting up folder structure

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+const expressLayout = require("express-ejs-layouts");
+
+app.use(express.static("./assets"));
+app.use(expressLayout);
+//extract style and script from sub pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
 //use express router
 app.use("/", require("./routes"));
+
+//setup the view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.listen(port, function (err) {
   if (err) {
